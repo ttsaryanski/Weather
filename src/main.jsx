a70811fd0a81ab4199ef4258916fffa38d73b80a
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2025-06-23 17:33:32 +0300

    Add PWA

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 
+import { registerSW } from "virtual:pwa-register";
+
 import App from "./App.jsx";
+
+registerSW();
 
 createRoot(document.getElementById("root")).render(
     // <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: "autoUpdate",
+            manifest: {
+                name: "Weather App",
+                short_name: "Weather",
+                start_url: "/",
+                display: "standalone",
+                background_color: "#ffffff",
+                theme_color: "#2c3e50",
+            },
+        }),
+    ],
     base: "/",
+    server: {
+        historyApiFallback: true,
+    },
 });
